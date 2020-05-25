c39a5ddc9ccb41833080c212b03d10058eea6484
Lotte van 't Hof <s1112052@student.hsleiden.nl>
2020-05-25 11:32:56 +0200

    begin van login scherm met goede kleuren

--- app/src/main/java/com/example/cocktailtime/MainActivity.java
+++ app/src/main/java/com/example/cocktailtime/MainActivity.java
@@ -2,13 +2,51 @@ package com.example.cocktailtime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText Name;
+    private EditText Password;
+    private TextView Info;
+    private Button Login;
+    private int counter = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Name = (EditText)findViewById(R.id.etName);
+        Password = (EditText)findViewById(R.id.etEmail);
+        Info = (TextView)findViewById(R.id.tvInfo);
+        Login = (Button)findViewById(R.id.btnRegister);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(Name.getText().toString(),Password.getText().toString());
+
+            }
+        });
+
+    }
+
+    private void validate(String userName, String userPassword){
+        if((userName.equals("Lotte")) && (userPassword.equals("lotte"))){
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        }else{
+
+            Info.setText("The credentials you are using are invalid!");
+
+
+
+        }
     }
 }
